a3b9ed8dfe388f3dc5788135c820188b76b19487
Andres Ramirez <Andres Ramirez@DESKTOP-0OGE88F.bbrouter>
2024-03-29 16:33:24 -0500

    clase

--- EjerciciosFundamentos/src/Ejercicios/EjerciciosClase.java
+++ EjerciciosFundamentos/src/Ejercicios/EjerciciosClase.java
@@ -10,9 +10,34 @@ public class EjerciciosClase {
 		
 		//2
 		
-		int mult, suma, resta, div;
+		//int mult, suma, resta, div;
 		
 		System.out.println("Multiplicacion: " + (a*b));
+		System.out.println("Suma: " + (a+b));
+		System.out.println("Resta: " + (a-b));
+		System.out.println("Division: " + (a/b));
+		
+		//3
+		
+		a++;
+		b--;
+		
+		//4
+		
+		a = a+2;
+		b = b*3;
+		
+		//5
+		
+		if (a == b) {
+			System.out.println("Igual");
+		}else {
+			System.out.println("Diferentes");
+		}if (a > b) {
+			System.out.println(a + "es mayor que " + b);
+		}else {
+			System.out.println(a + "es menor que " + b);
+		}
 	}
 
 }
